f3f674d653a0685fa55fd6895b5657a8bfb4e3cc
deadlydog <deadlydog@hotmail.com>
2018-04-29 01:52:09 -0600

    Breaking code out into a module and just have task.ps1 call it

---
+++ src/Code/Uninstall-WindowsScheduledTask.psm1
@@ -0,0 +1,84 @@
+#Requires -Version 3.0
+#Requires -RunAsAdministrator
+
+function Uninstall-WindowsScheduledTask
+{
+	param
+	(
+		[parameter(Mandatory=$true,HelpMessage="The name of the Windows Scheduled Task to uninstall.")]
+		[ValidateNotNullOrEmpty()]
+		[string] $ScheduledTaskName,
+
+		[parameter(Mandatory=$false,HelpMessage="List of the computer(s) to uninstall the scheduled task from. If null localhost will be used.")]
+		[string[]] $ComputerName,
+
+		[parameter(Mandatory=$false,HelpMessage="The credential to use to connect to the computer(s).")]
+		[PSCredential] $Credential
+	)
+
+	Process
+	{
+		[hashtable] $scheduledTaskSettings = @{
+			TaskName = $ScheduledTaskName
+		}
+
+		Invoke-WindowsScheduledTaskUninstallOnComputers -scheduledTaskSettings $scheduledTaskSettings -computers $ComputerName -credential $Credential
+	}
+
+	Begin
+	{
+		function Invoke-WindowsScheduledTaskUninstallOnComputers([hashtable] $scheduledTaskSettings, [string[]] $computers, [PSCredential] $credential)
+		{
+			[bool] $noComputersWereSpecified = ($computers -eq $null -or $computers.Count -eq 0)
+			[bool] $noCredentialWasSpecified = ($credential -eq $null)
+
+			if ($noComputersWereSpecified)
+			{
+				if ($noCredentialWasSpecified)
+				{
+					Invoke-Command -ScriptBlock $uninstallScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose
+				}
+				else
+				{
+					Invoke-Command -Credential $credential -ScriptBlock $uninstallScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose
+				}
+			}
+			else
+			{
+				if ($noCredentialWasSpecified)
+				{
+					Invoke-Command -ComputerName $computers -ScriptBlock $uninstallScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose
+				}
+				else
+				{
+					Invoke-Command -ComputerName $computers -Credential $credential -ScriptBlock $uninstallScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose
+				}
+			}
+		}
+
+		$uninstallScheduledTaskScriptBlock = {
+			param ([hashtable] $scheduledTaskSettings)
+			[string] $computerName = $Env:COMPUTERNAME
+			[string] $username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+			Write-Verbose "Connected to computer '$computerName' as user '$username'." -Verbose
+
+			$taskNameParts = $scheduledTaskSettings.TaskName -split '\\'
+			$taskName = $taskNameParts | Select-Object -Last 1
+			$taskPath = '\' + $scheduledTaskSettings.TaskName.Substring(0, $scheduledTaskSettings.TaskName.Length - $taskName.Length)
+
+			$task = Get-ScheduledTask -TaskName $taskName -TaskPath $taskPath -ErrorAction SilentlyContinue
+			if ($task -eq $null)
+			{
+				Write-Host "A scheduled task with the path '$taskPath' and name '$taskName' was not found on computer '$computerName', so no scheduled tasks will be uninstalled."
+				return
+			}
+
+			Write-Host "Uninstalling Scheduled Task '$taskName' on computer '$computerName'."
+			$task | Disable-ScheduledTask
+			$task | Stop-ScheduledTask
+			$task | Unregister-ScheduledTask -Confirm:$false
+		}
+	}
+}
+
+Export-ModuleMember -Function Uninstall-WindowsScheduledTask
--- src/Tasks/UninstallWindowsScheduledTask/task.ps1
+++ src/Tasks/UninstallWindowsScheduledTask/task.ps1
@@ -2,112 +2,41 @@ param
 (
 	[parameter(Mandatory=$true,HelpMessage="The name of the Windows Scheduled Task to uninstall.")]
 	[ValidateNotNullOrEmpty()]
-	[string] $scheduledTaskName,
+	[string] $ScheduledTaskName,
 
 	[parameter(Mandatory=$false,HelpMessage="Comma-separated list of the computer(s) to uninstall the scheduled task from.")]
 	[ValidateNotNullOrEmpty()]
-	[string] $computerNames,
+	[string] $ComputerNames,
 
 	[parameter(Mandatory=$false,HelpMessage="The username to use to connect to the computer(s).")]
-	[string] $username,
+	[string] $Username,
 
 	[parameter(Mandatory=$false,HelpMessage="The password to use to connect to the computer(s).")]
-	[string] $password
+	[string] $Password
 )
 
 Process
 {
-	Write-Verbose "About to uninstall Windows Scheduled Task '$scheduledTaskName' on '$computerNames'." -Verbose
+	Write-Verbose "About to attempt to uninstall Windows Scheduled Task '$ScheduledTaskName' on '$ComputerNames'." -Verbose
 
-	[string[]] $computers = Get-ComputersToConnectToOrNull -computerNames $computerNames
-	[PSCredential] $credential = Convert-UsernameAndPasswordToCredentialsOrNull -username $username -password $password
+	[string[]] $computers = Get-ComputersToConnectToOrNull -computerNames $ComputerNames
+	[PSCredential] $credential = Convert-UsernameAndPasswordToCredentialsOrNull -username $Username -password $Password
 
-	[hashtable] $scheduledTaskSettings = @{
-		TaskName = $scheduledTaskName
-	}
-
-	Invoke-WindowsScheduledTaskUninstallOnComputers -scheduledTaskSettings $scheduledTaskSettings -computers $computers -credential $credential
+	Uninstall-WindowsScheduledTask -ScheduledTaskName $ScheduledTaskName -ComputerName $computers -Credential $credential
 }
 
 Begin
 {
-	function Get-ComputersToConnectToOrNull([string] $computerNames)
-	{
-# TODO - Write unit test to see if $null or an empty array are returned when no computersNames are given
-		[string[]] $computers = $computerNames -split ','
+	# Build paths to files/modules to import, and then import them.
+	$THIS_SCRIPTS_DIRECTORY_PATH = $PSScriptRoot
+	$srcDirectoryPath = Split-Path -Path (Split-Path -Path $THIS_SCRIPTS_DIRECTORY_PATH -Parent) -Parent
+	$codeDirectoryPath = Join-Path -Path $srcDirectoryPath -ChildPath 'Code'
 
-		[bool] $arrayContainsOneBlankElement = ($computers.Count -eq 1 -and [string]::IsNullOrWhiteSpace($computers[0]))
-		if ($arrayContainsOneBlankElement)
-		{
-			# $computers = [string[]]::new(0)
-			$computers = $null
-		}
+	$utilitiesFilePath = Join-Path -Path $codeDirectoryPath -ChildPath 'Utilities.ps1'
+	Write-Verbose "Importing the file '$utilitiesFilePath'." -Verbose
+	. $utilitiesFilePath
 
-		return $computers
-	}
-
-	function Convert-UsernameAndPasswordToCredentialsOrNull([string] $username, [string] $password)
-	{
-		if ([string]::IsNullOrWhiteSpace($username))
-		{
-			return $null
-		}
-# TODO - Write unit test to see what happens with a blank password
-		[SecureString] $securePassword = ($password | ConvertTo-SecureString -AsPlainText -Force)
-
-		$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username,$securePassword
-		return $credential
-	}
-
-	function Invoke-WindowsScheduledTaskUninstallOnComputers([hashtable] $scheduledTaskSettings, [string[]] $computers, [PSCredential] $credential)
-	{
-		[bool] $noComputersWereSpecified = ($computers -eq $null -or $computers.Count -eq 0)
-		[bool] $noCredentialWasSpecified = ($credential -eq $null)
-
-		if ($noComputersWereSpecified)
-		{
-			if ($noCredentialWasSpecified)
-			{
-				Invoke-Command -ScriptBlock $uninstallScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose
-			}
-			else
-			{
-				Invoke-Command -Credential $credential -ScriptBlock $uninstallScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose
-			}
-		}
-		else
-		{
-			if ($noCredentialWasSpecified)
-			{
-				Invoke-Command -ComputerName $computers -ScriptBlock $uninstallScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose
-			}
-			else
-			{
-				Invoke-Command -ComputerName $computers -Credential $credential -ScriptBlock $uninstallScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose
-			}
-		}
-	}
-
-	$uninstallScheduledTaskScriptBlock = {
-		param ([hashtable] $scheduledTaskSettings)
-		[string] $computerName = $Env:COMPUTERNAME
-		[string] $username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-		Write-Verbose "Connected to computer '$computerName' as user '$username'." -Verbose
-
-		$taskNameParts = $scheduledTaskSettings.TaskName -split '\\'
-		$taskName = $taskNameParts | Select-Object -Last 1
-		$taskPath = '\' + $scheduledTaskSettings.TaskName.Substring(0, $scheduledTaskSettings.TaskName.Length - $taskName.Length)
-
-		$task = Get-ScheduledTask -TaskName $taskName -TaskPath $taskPath -ErrorAction SilentlyContinue
-		if ($task -eq $null)
-		{
-			Write-Host "A scheduled task with the path '$taskPath' and name '$taskName' was not found on computer '$computerName', so no scheduled tasks will be uninstalled."
-			return
-		}
-
-		Write-Host "Uninstalling Scheduled Task '$taskName' on computer '$computerName'."
-		$task | Disable-ScheduledTask
-		$task | Stop-ScheduledTask
-		$task | Unregister-ScheduledTask -Confirm:$false
-	}
+	$uninstallWindowsScheduledTaskFilePath = Join-Path -Path $codeDirectoryPath -ChildPath 'Uninstall-WindowsScheduledTask.psm1'
+	Write-Verbose "Importing the module '$uninstallWindowsScheduledTaskFilePath'." -Verbose
+	Import-Module -Name $uninstallWindowsScheduledTaskFilePath -Force
 }
