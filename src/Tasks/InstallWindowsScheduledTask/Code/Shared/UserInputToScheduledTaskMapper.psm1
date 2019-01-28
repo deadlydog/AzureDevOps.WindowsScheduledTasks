9b0f61fabcf8c5e7c92b377c33e80fcc18e13f2c
Daniel Schroeder <deadlydog@hotmail.com>
2019-01-28 01:24:50 -0600

    Added some of the code for creating the Scheduled Trigger, but still not done yet

--- src/Tasks/InstallWindowsScheduledTask/Code/Shared/UserInputToScheduledTaskMapper.psm1
+++ src/Tasks/InstallWindowsScheduledTask/Code/Shared/UserInputToScheduledTaskMapper.psm1
@@ -62,6 +62,88 @@ function Get-WorkingDirectory
 	return $workingDirectory
 }
 
+function Get-ScheduledTaskTrigger
+{
+	param
+	(
+		[ValidateSet('DateTime', 'AtLogOn', 'AtStartup')]
+		[string] $triggerType,	# TEMP: parameter accounted for in function.
+		[string] $atLogOnTriggerUsername,	# TEMP: parameter accounted for in function.
+		[string] $dateTimeScheduleStartTime,	# TEMP: parameter accounted for in function.
+		[string] $dateTimeScheduleFrequencyOptions,	# TEMP: parameter accounted for in function.
+		[string] $dateTimeScheduleFrequencyDailyInterval,
+		[string] $dateTimeScheduleFrequencyWeeklyInterval,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunMulipleTimesAWeek,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnMondays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnTuesdays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnWednesdays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnThursdays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnFridays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnSaturdays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnSundays,
+		[bool] $shouldScheduledTaskRunRepeatedly,	# TEMP: parameter accounted for in function.
+		[string] $scheduleRepetitionIntervalInMinutes,	# TEMP: parameter accounted for in function.
+		[string] $scheduleRepetitionDurationInMinutes,	# TEMP: parameter accounted for in function.
+		[string] $scheduleStartTimeRandomDelayInMinutes	# TEMP: parameter accounted for in function.
+	)
+
+	[string] $createTriggerExpression = 'New-ScheduledTaskTrigger'
+
+	switch ($triggerType)
+	{
+		'AtLogOn' {
+			$createTriggerExpression += ' -AtLogOn'
+			$createTriggerExpression += " -User '$atLogOnTriggerUser'"
+		}
+
+		'AtStartup' {
+			$createTriggerExpression += ' -AtStartup'
+		}
+
+		'DateTime' {
+			$createTriggerExpression += " -At $dateTimeScheduleStartTime"
+
+# TODO: finsih this
+			case ($dateTimeScheduleFrequencyOptions)
+			{
+				'Once' {
+					$createTriggerExpression += ' -Once'
+				}
+
+				'Daily' {
+					$createTriggerExpression += ' -Daily'
+
+				}
+
+				'Weekly' {
+					$createTriggerExpression += ' -Weekly'
+
+				}
+			}
+		}
+	}
+
+	if (!([string]::IsNullOrWhiteSpace($scheduleStartTimeRandomDelayInMinutes))
+	{
+		ConvertMinutesToTimeSpanAndAddParameterToExpression -expression $createTriggerExpression -parameterName 'RandomDelay' -minutes $scheduleStartTimeRandomDelayInMinutes
+	}
+
+	if ($shouldScheduledTaskRunRepeatedly)
+	{
+		ConvertMinutesToTimeSpanAndAddParameterToExpression -expression $createTriggerExpression -parameterName 'RepetitionInterval' -minutes $scheduleRepetitionIntervalInMinutes
+		ConvertMinutesToTimeSpanAndAddParameterToExpression -expression $createTriggerExpression -parameterName 'RepetitionDuration' -minutes $scheduleRepetitionDurationInMinutes
+	}
+}
+
+function ConvertMinutesToTimeSpanAndAddParameterToExpression([string] $expression, [string] $parameterName, [string] $minutes)
+{
+	[double] $minutesAsDouble = [double]::Parse($scheduleStartTimeRandomDelayInMinutes)
+	[timespan] $minutesAsTimeSpan = [timespan]::FromMinutes($randomDelayInMinutes)
+	[string] $minutesTimeSpanAsString = $minutesAsTimeSpan.ToString()
+	$createTriggerExpression += " -$parameterName $minutesTimeSpanAsString"
+}
+
 Export-ModuleMember -Function Get-ScheduledTaskNameAndPath
 Export-ModuleMember -Function Get-AccountCredentialsToRunScheduledTaskAs
 Export-ModuleMember -Function Get-WorkingDirectory
+Export-ModuleMember -Function Get-ScheduledTaskTrigger
