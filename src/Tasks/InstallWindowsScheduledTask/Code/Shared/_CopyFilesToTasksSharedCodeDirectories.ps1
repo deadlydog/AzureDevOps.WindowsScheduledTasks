4c54178364c2ee3612f6eb8145d2670caf83e29c
Daniel Schroeder <deadlydog@hotmail.com>
2019-02-11 02:15:57 -0600

    chore: Copy files from new "root" shared files to tasks

---
+++ src/Tasks/InstallWindowsScheduledTask/Code/Shared/_CopyFilesToTasksSharedCodeDirectories.ps1
@@ -0,0 +1,22 @@
+[string] $THIS_SCRIPTS_DIRECTORY_PATH = $PSScriptRoot
+[string] $thisScriptsDirectoryName = Split-Path -Path $THIS_SCRIPTS_DIRECTORY_PATH -Leaf
+if ($thisScriptsDirectoryName -ne 'SharedTaskCode')
+{
+	throw "This script should only be ran from the SharedTaskCode directory, not a specific task's directory. Directory this is being rane from is '$THIS_SCRIPTS_DIRECTORY_PATH'."
+}
+
+[string] $srcDirectoryPath = Split-Path -Path $THIS_SCRIPTS_DIRECTORY_PATH -Parent
+[string[]] $tasksSharedCodeDirectoryPaths = @(
+	'Tasks\InstallWindowsScheduledTask\Code\Shared'
+	'Tasks\UninstallWindowsScheduledTask\Code\Shared'
+)
+
+$tasksSharedCodeDirectoryPaths | ForEach-Object {
+	$tasksLocalSharedCodeDirectoryPath = $_
+	$tasksDirectoryToCopyFilesTo = Join-Path -Path $srcDirectoryPath -ChildPath $tasksLocalSharedCodeDirectoryPath -Resolve
+
+	Write-Verbose "Copying files from '$THIS_SCRIPTS_DIRECTORY_PATH' to '$tasksDirectoryToCopyFilesTo'." -Verbose
+	Get-ChildItem -Path $THIS_SCRIPTS_DIRECTORY_PATH -Recurse -Force | Copy-Item -Destination $tasksDirectoryToCopyFilesTo -Force
+}
+
+Write-Output "Completed copying files"
