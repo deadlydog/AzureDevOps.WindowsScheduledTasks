91e2b4fefd2d122cedadaa6179f5fe69d99a3014
Daniel Schroeder <deadlydog@hotmail.com>
2019-02-25 23:13:02 -0600

    Copied files and renamed them to make new Start and Stop tasks, but not modified yet.

---
+++ src/Tasks/StartWindowsScheduledTask/Code/Shared/UserInputToScheduledTaskMapper.psm1
@@ -0,0 +1,213 @@
+# This file should only be modified if it is the one in the `SharedTaskCode` directory.
+# Otherwise changes here will be overwritten the next time the Copy Files script is ran.
+
+function Get-ScheduledTaskNameAndPath([string] $fullTaskName)
+{
+	$fullTaskName = $fullTaskName.TrimStart('\\')
+	[string[]] $taskNameParts = $fullTaskName -split '\\'
+	[string] $taskName = $taskNameParts | Select-Object -Last 1
+	[string] $taskPath = '\' + $fullTaskName.Substring(0, $fullTaskName.Length - $taskName.Length)
+
+	[hashtable] $taskNameAndPath = @{
+		Name = $taskName
+		Path = $taskPath
+	}
+	return $taskNameAndPath
+}
+
+function Get-AccountCredentialsToRunScheduledTaskAs
+{
+	param
+	(
+		[ValidateSet('System', 'LocalService', 'NetworkService', 'CustomAccount')]
+		[string] $scheduldTaskAccountToRunAsOptions,
+		[string] $customAccountToRunScheduledTaskAsUsername,
+		[string] $customAccountToRunScheduledTaskAsPassword
+	)
+
+	[string] $username = [string]::Empty
+	[string] $password = [string]::Empty
+	switch ($scheduldTaskAccountToRunAsOptions)
+	{
+		"System" { $username = 'NT AUTHORITY\SYSTEM'; break }
+		"LocalService" { $username = 'NT AUTHORITY\LOCALSERVICE'; break }
+		"NetworkService" { $username = 'NT AUTHORITY\NETWORKSERVICE'; break }
+		default
+		{
+			$username = $customAccountToRunScheduledTaskAsUsername
+			$password = $customAccountToRunScheduledTaskAsPassword
+			break
+		}
+	}
+
+	[hashtable] $accountCredentials = @{
+		Username = $username
+		Password = $password
+	}
+	return $accountCredentials
+}
+
+function Get-WorkingDirectory
+{
+	param
+	(
+		[ValidateSet('ApplicationDirectory', 'CustomDirectory')]
+		[string] $workingDirectoryOption,
+		[string] $customWorkingDirectory,
+		[string] $applicationPath
+	)
+
+	[string] $workingDirectory = $customWorkingDirectory
+	if ($workingDirectoryOption -eq 'ApplicationDirectory')
+	{
+		$workingDirectory = Split-Path -Path $applicationPath -Parent
+	}
+	return $workingDirectory
+}
+
+function Get-ScheduledTaskAction([string] $applicationPathToRun, [string] $applicationArguments, [string] $workingDirectory)
+{
+	$createActionExpression = "New-ScheduledTaskAction -Execute `"$applicationPathToRun`""
+
+	if (!([string]::IsNullOrWhiteSpace($applicationArguments)))
+	{
+		$createActionExpression += " -Argument `"$applicationArguments`""
+	}
+
+	if (!([string]::IsNullOrWhiteSpace($workingDirectory)))
+	{
+		$createActionExpression += " -WorkingDirectory `"$workingDirectory`""
+	}
+
+	[CimInstance[]] $scheduledTaskAction = Invoke-Expression -Command $createActionExpression
+	return $scheduledTaskAction
+}
+
+function Get-ScheduledTaskTrigger
+{
+	param
+	(
+		[ValidateSet('DateTime', 'AtLogOn', 'AtStartup')]
+		[string] $triggerType,
+		[string] $atLogOnTriggerUsername,
+		[string] $dateTimeScheduleStartTime,
+		[string] $dateTimeScheduleFrequencyOptions,
+		[string] $dateTimeScheduleFrequencyDailyInterval,
+		[string] $dateTimeScheduleFrequencyWeeklyInterval,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnMondays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnTuesdays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnWednesdays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnThursdays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnFridays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnSaturdays,
+		[bool] $shouldDateTimeScheduleFrequencyWeeklyRunOnSundays,
+		[bool] $shouldScheduledTaskRunRepeatedly,
+		[string] $scheduleRepetitionIntervalInMinutes,
+		[string] $scheduleRepetitionDurationInMinutes,
+		[string] $scheduleStartTimeRandomDelayInMinutes
+	)
+
+	[string] $createTriggerExpression = 'New-ScheduledTaskTrigger'
+
+	switch ($triggerType)
+	{
+		'AtLogOn' {
+			$createTriggerExpression += ' -AtLogOn'
+			$createTriggerExpression += " -User '$atLogOnTriggerUsername'"
+			break
+		}
+
+		'AtStartup' {
+			$createTriggerExpression += ' -AtStartup'
+			break
+		}
+
+		'DateTime' {
+			$createTriggerExpression += " -At '$dateTimeScheduleStartTime'"
+
+			switch ($dateTimeScheduleFrequencyOptions)
+			{
+				'Once' {
+					$createTriggerExpression += ' -Once'
+				}
+
+				'Daily' {
+					$createTriggerExpression += ' -Daily'
+					$createTriggerExpression += " -DaysInterval $dateTimeScheduleFrequencyDailyInterval"
+				}
+
+				'Weekly' {
+					$createTriggerExpression += ' -Weekly'
+					$createTriggerExpression += " -WeeksInterval $dateTimeScheduleFrequencyWeeklyInterval"
+
+					[System.DayOfWeek[]] $daysOfTheWeekToRunOn = @()
+					if ($shouldDateTimeScheduleFrequencyWeeklyRunOnMondays) { $daysOfTheWeekToRunOn += [System.DayOfWeek]::Monday }
+					if ($shouldDateTimeScheduleFrequencyWeeklyRunOnTuesdays) { $daysOfTheWeekToRunOn += [System.DayOfWeek]::Tuesday }
+					if ($shouldDateTimeScheduleFrequencyWeeklyRunOnWednesdays) { $daysOfTheWeekToRunOn += [System.DayOfWeek]::Wednesday }
+					if ($shouldDateTimeScheduleFrequencyWeeklyRunOnThursdays) { $daysOfTheWeekToRunOn += [System.DayOfWeek]::Thursday }
+					if ($shouldDateTimeScheduleFrequencyWeeklyRunOnFridays) { $daysOfTheWeekToRunOn += [System.DayOfWeek]::Friday }
+					if ($shouldDateTimeScheduleFrequencyWeeklyRunOnSaturdays) { $daysOfTheWeekToRunOn += [System.DayOfWeek]::Saturday }
+					if ($shouldDateTimeScheduleFrequencyWeeklyRunOnSundays) { $daysOfTheWeekToRunOn += [System.DayOfWeek]::Sunday }
+					[string] $daysOfTheWeekToRunOnString = $daysOfTheWeekToRunOn -join ','
+					$createTriggerExpression += " -DaysOfWeek $daysOfTheWeekToRunOnString"
+
+					if ([string]::IsNullOrWhiteSpace($daysOfTheWeekToRunOnString))
+					{
+						throw "When using a Weekly DateTime trigger, you must specify at least one day of the week (Monday - Sunday) that the Scheduled Task should run on."
+					}
+				}
+			}
+			break
+		}
+	}
+
+	if (!([string]::IsNullOrWhiteSpace($scheduleStartTimeRandomDelayInMinutes)))
+	{
+		ConvertMinutesToTimeSpanAndAddParameterToExpression -expression $createTriggerExpression -parameterName 'RandomDelay' -minutes $scheduleStartTimeRandomDelayInMinutes
+	}
+
+	if ($shouldScheduledTaskRunRepeatedly)
+	{
+		ConvertMinutesToTimeSpanAndAddParameterToExpression -expression $createTriggerExpression -parameterName 'RepetitionInterval' -minutes $scheduleRepetitionIntervalInMinutes
+		ConvertMinutesToTimeSpanAndAddParameterToExpression -expression $createTriggerExpression -parameterName 'RepetitionDuration' -minutes $scheduleRepetitionDurationInMinutes
+	}
+
+	[CimInstance[]] $scheduledTaskTrigger = Invoke-Expression -Command $createTriggerExpression
+	return $scheduledTaskTrigger
+}
+
+function ConvertMinutesToTimeSpanAndAddParameterToExpression([string] $expression, [string] $parameterName, [string] $minutes)
+{
+	[double] $minutesAsDouble = [double]::Parse($minutes)
+	[timespan] $minutesAsTimeSpan = [timespan]::FromMinutes($minutesAsDouble)
+	[string] $minutesTimeSpanAsString = $minutesAsTimeSpan.ToString()
+	$expression += " -$parameterName $minutesTimeSpanAsString"
+}
+
+function Get-ScheduledTaskSettings([bool] $shouldBeEnabled)
+{
+	[string] $createSettingsExpression = "New-ScheduledTaskSettingsSet"
+
+	if (!($shouldBeEnabled)) { $createSettingsExpression += ' -Disable' }
+
+	[CimInstance] $scheduledTaskSettings = Invoke-Expression -Command $createSettingsExpression
+	return $scheduledTaskSettings
+}
+
+function Get-ScheduledTaskRunLevel([bool] $shouldScheduledTaskRunWithHighestPrivileges)
+{
+	[string] $privileges = 'Limited'
+	if ($shouldScheduledTaskRunWithHighestPrivileges)
+	{
+		$privileges = 'Highest'
+	}
+	return $privileges
+}
+
+Export-ModuleMember -Function Get-ScheduledTaskNameAndPath
+Export-ModuleMember -Function Get-AccountCredentialsToRunScheduledTaskAs
+Export-ModuleMember -Function Get-WorkingDirectory
+Export-ModuleMember -Function Get-ScheduledTaskAction
+Export-ModuleMember -Function Get-ScheduledTaskTrigger
+Export-ModuleMember -Function Get-ScheduledTaskSettings
+Export-ModuleMember -Function Get-ScheduledTaskRunLevel
---
+++ src/Tasks/StopWindowsScheduledTask/Code/Stop-WindowsScheduledTask.psm1
@@ -0,0 +1,95 @@
+#Requires -Version 3.0
+#Requires -RunAsAdministrator
+
+function Enable-WindowsScheduledTask
+{
+	[CmdletBinding()]
+	param
+	(
+		[parameter(Mandatory=$true,HelpMessage="The name of the Windows Scheduled Task to enable.")]
+		[ValidateNotNullOrEmpty()]
+		[string] $ScheduledTaskName,
+
+		[parameter(Mandatory=$true,HelpMessage="The path in the Task Scheduler of the Windows Scheduled Task to be enabled.")]
+		[ValidateNotNullOrEmpty()]
+		[string] $ScheduledTaskPath,
+
+		[parameter(Mandatory=$false,HelpMessage="List of the computer(s) to enable the scheduled task on. If null localhost will be used.")]
+		[string[]] $ComputerName,
+
+		[parameter(Mandatory=$false,HelpMessage="The credential to use to connect to the computer(s).")]
+		[PSCredential] $Credential,
+
+		[parameter(Mandatory=$false,HelpMessage="If CredSSP should be used when connecting to remote computers or not.")]
+		[bool] $UseCredSsp
+	)
+
+	Process
+	{
+		[hashtable] $scheduledTaskSettings = @{
+			ScheduledTaskName = $ScheduledTaskName
+			ScheduledTaskPath = $ScheduledTaskPath
+		}
+
+		Invoke-EnableWindowsScheduledTaskFromComputers -scheduledTaskSettings $scheduledTaskSettings -computers $ComputerName -credential $Credential -useCredSsp $UseCredSsp
+	}
+
+	Begin
+	{
+		function Invoke-EnableWindowsScheduledTaskFromComputers([hashtable] $scheduledTaskSettings, [string[]] $computers, [PSCredential] $credential, [bool] $useCredSsp)
+		{
+			[string] $enableTaskCommand = 'Invoke-Command -ScriptBlock $enableScheduledTaskScriptBlock -ArgumentList $scheduledTaskSettings -Verbose'
+
+			[bool] $computersWereSpecified = ($null -ne $computers -and $computers.Count -gt 0)
+			if ($computersWereSpecified)
+			{
+				$enableTaskCommand += ' -ComputerName $computers'
+			}
+
+			[bool] $credentialWasSpecified = ($null -ne $credential)
+			if ($credentialWasSpecified)
+			{
+				$enableTaskCommand += ' -Credential $credential'
+			}
+
+			if ($useCredSsp)
+			{
+				$enableTaskCommand += ' -Authentication Credssp'
+			}
+
+			[string] $enableTaskCommandWithVariablesExpanded = $ExecutionContext.InvokeCommand.ExpandString($enableTaskCommand)
+			Write-Debug "About to invoke expression '$enableTaskCommandWithVariablesExpanded'."
+			Invoke-Expression -Command $enableTaskCommand -Verbose
+		}
+
+		[scriptblock] $enableScheduledTaskScriptBlock = {
+			param ([hashtable] $scheduledTaskSettings)
+			[string] $computerName = $Env:COMPUTERNAME
+			[string] $username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+			[string] $operatingSystemVersion = [System.Environment]::OSVersion
+			[string] $powerShellVersion = $PSVersionTable.PSVersion
+			Write-Verbose "Connected to computer '$computerName' as user '$username'. It is running operating system '$operatingSystemVersion' and PowerShell version '$powerShellVersion'." -Verbose
+
+			[string] $taskName = $scheduledTaskSettings.ScheduledTaskName
+			[string] $taskPath = $scheduledTaskSettings.ScheduledTaskPath
+
+			Write-Verbose "Searching for a Scheduled Task with the path '$taskPath' and name '$taskName'." -Verbose
+			$tasks = Get-ScheduledTask -TaskName $taskName -TaskPath $taskPath -ErrorAction SilentlyContinue
+			if ($null -eq $tasks)
+			{
+				[string] $taskPathAndName = $taskPath + $taskName
+				Write-Warning "A Scheduled Task matching the path and name '$taskPathAndName' was not found on computer '$computerName', so no scheduled tasks will be enabled."
+				return
+			}
+
+			foreach ($task in $tasks)
+			{
+				[string] $taskPathAndName = $task.TaskPath + $task.TaskName
+				Write-Output "Enabling Scheduled Task '$taskPathAndName' on computer '$computerName'."
+				$task | Enable-ScheduledTask
+			}
+		}
+	}
+}
+
+Export-ModuleMember -Function Enable-WindowsScheduledTask
