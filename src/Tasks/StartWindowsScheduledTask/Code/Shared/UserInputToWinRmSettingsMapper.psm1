d38618437c621c9c13e7b8143d01f677d72b9a35
Daniel Schroeder <deadlydog@hotmail.com>
2019-05-15 00:41:59 -0600

    Copy shared files into each task directory

--- src/Tasks/StartWindowsScheduledTask/Code/Shared/UserInputToWinRmSettingsMapper.psm1
+++ src/Tasks/StartWindowsScheduledTask/Code/Shared/UserInputToWinRmSettingsMapper.psm1
@@ -1,6 +1,32 @@
 # This file should only be modified if it is the one in the `SharedTaskCode` directory.
 # Otherwise changes here will be overwritten the next time the Copy Files script is ran.
 
+function Get-ComputersToConnectToOrNull([string] $computerNames)
+{
+	[string[]] $computers = $computerNames -split '\s*,\s*'
+
+	[bool] $arrayContainsOneBlankElement = ($computers.Count -eq 1 -and [string]::IsNullOrWhiteSpace($computers[0]))
+	if ($arrayContainsOneBlankElement)
+	{
+		$computers = $null
+	}
+
+	return $computers
+}
+
+function Convert-UsernameAndPasswordToCredentialsOrNull([string] $username, [string] $password)
+{
+	if ([string]::IsNullOrWhiteSpace($username) -or [string]::IsNullOrWhiteSpace($password))
+	{
+		return $null
+	}
+
+	[SecureString] $securePassword = ($password | ConvertTo-SecureString -AsPlainText -Force)
+
+	$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $securePassword
+	return $credential
+}
+
 function Get-WinRmSettings
 {
 	param
@@ -38,4 +64,6 @@ function Get-WinRmSettings
 	return $winRmSettings
 }
 
+Export-ModuleMember -Function Get-ComputersToConnectToOrNull
+Export-ModuleMember -Function Convert-UsernameAndPasswordToCredentialsOrNull
 Export-ModuleMember -Function Get-WinRmSettings
